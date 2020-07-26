5585ba11a9f8f7e593366509ada0bcc1b7c56879
Babz_Uni <haastrub@uni.coventry.ac.uk>
2020-07-26 02:49:45 +0100

    Base feature set done, just need to add search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Row, Col, Divider } from 'antd'
+import { Row, Col, Switch } from 'antd'
+import CountryCard from './components/countrycard/countrycard'
 import 'antd/dist/antd.css';
 
 const style = { background: '#0092ff', padding: '8px 0' };
@@ -11,78 +12,113 @@ export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state= {
-
+      isLoaded: false,
+      items:[],
+      text: "text",
+      visible: false,
+      bgcolor: 'grey',
     }
+    this.test = "go"
   }
+
+  
+  componentDidMount() {
+    fetch("http://localhost:8080/https://restcountries.eu/rest/v2/all", {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(res => res.json())
+      .then(
+        (result) => {
+          console.log("IT worked OK")
+        this.setState({
+          isLoaded: true,
+          items: result
+        });
+      },
+    (error) => {
+      console.log("didnt work")
+      console.log(error)
+      this.setState({
+        isLoaded: true,
+        error
+      })
+    }
+    )
+    this.cole()
+  }
+
+  cole(){
+    //console.log(this.state.text)
+    console.log(this.test)
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    })
+  }
+
+  setColor = () => {
+    if (this.state.bgcolor === 'white') {
+    this.setState({
+      bgcolor: 'grey',
+    })
+  } else {
+    this.setState({
+      bgcolor: 'white',
+    })
+  }
+  }
+
+  oneRow(countries, rowNumber){
+    let source = `https://www.countryflags.io/be/flat/64.png`
+    let row = countries.map((element, index)=> {
+      return  <Col span={6} style={{ padding: '20px'}}  key={index}>
+        {element !== null ? (
+          
+          <CountryCard 
+            title={"Click to get more details"} name={element.name} capital={element.capital} currencies={element.currencies.map(currency => <div>{currency.name}</div>)} 
+            populationCount={element.population} isoCode={element.alpha3Code} languages={element.languages.map(language => <div>{language.name}</div>)}  
+            imgURL={`https://www.countryflags.io/${element.alpha2Code}/flat/64.png`} />) : null}
+         
+          </Col>
+          
+        }
+    );
+
+    return <div key={rowNumber}>
+      <Row type = "flex" justify="center">
+        {row}
+      </Row>
+    </div>
+    }
+
   render (){
+    let allRows = []
+    let counter = 1
+    let rowNumber = 0;
+    while(counter < this.state.items.length){
+      let countriesPerRow = []
+      for (let i = 0; i < 3; i++){
+        if(counter < this.state.items.length){
+          countriesPerRow.push(this.state.items[counter]);
+        } else {
+          countriesPerRow.push(null)
+        }
+        counter++
+      }
+      rowNumber++;
+      allRows.push(this.oneRow(countriesPerRow,rowNumber))
+    }
     return (
-      <>
-      <div style={divstyle}>
-        <Divider orientation="left" style={{ color: '#333', fontWeight: 'normal',padding: '12px' }}>
-          Horizontal
-        </Divider>
-        <Row gutter={16}>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-        </Row>
-        <Divider orientation="left" style={{ color: '#333', fontWeight: 'normal' }}>
-          Responsive
-        </Divider>
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-        </Row>
-        <Divider orientation="left" style={{ color: '#333', fontWeight: 'normal' }}>
-          Vertical
-        </Divider>
-        <Row gutter={[16, 24]}>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <div style={style}>col-6</div>
-          </Col>
-        </Row>
-        </div>
-      </>
+      <div style={{ backgroundColor: this.state.bgcolor}}>
+        <p>Click toggle below activate day/night mode</p>
+        <Switch checkedChildren="Night Mode" unCheckedChildren="Day Mode" onChange={this.setColor} defaultChecked> </Switch>
+        {allRows}
+      </div>
     )
   }
 }
---
+++ src/components/countrycard/countrycard.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Card, Modal } from 'antd'
+
+
+export default class CountryCard extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state= {
+            visible: false,
+        }
+      }
+      showModal = () => {
+        this.setState({
+          visible: true,
+        })
+      }
+    
+      handleOk = e => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+      };
+    
+      handleCancel = e => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+      };
+      render(){
+      
+          return (
+              <div>
+                    <Modal
+                        title="Country modal"
+                        visible={this.state.visible}
+                        onOk={this.handleOk}
+                        onCancel={this.handleCancel}
+                    >
+                        <p>Name: {this.props.name} </p>
+                        <p>Capital: {this.props.capital} </p>
+                        <p>Currencies: {this.props.currencies} </p>
+                        <p>Population Count: {this.props.populationCount} </p>
+                        <p>ISO Code: {this.props.isocode} </p>
+                        <p>Languages: {this.props.languages} </p>
+                        
+                    </Modal>
+                  <Card onClick={this.showModal} hoverable title={this.props.title}  cover={<img alt="example" src={this.props.imgURL} />}/>
+              </div>
+          )
+      }
+
+}
